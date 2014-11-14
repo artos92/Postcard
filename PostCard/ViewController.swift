74b10ad3f0018d640352be91a621ee12b4b84593
Артем Осипов <Artem@MacBookPro.local>
2014-11-15 01:58:51 +0300

    added nameLabel

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var sendMailButton: UIButton!
@@ -33,6 +34,13 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         messageLabel.hidden = false
+        
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        nameLabel.hidden = false;
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
  
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
